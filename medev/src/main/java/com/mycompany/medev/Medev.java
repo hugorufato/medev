47dc3cfd4caa7ab462c3c0828af9652c79a8103d
hugorufato <hugoc.rufato@usp.br>
2024-01-17 21:06:46 +0100

    'first'

---
+++ medev/src/main/java/com/mycompany/medev/Medev.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.medev;
+
+/**
+ *
+ * @author hugoc
+ */
+public class Medev {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
